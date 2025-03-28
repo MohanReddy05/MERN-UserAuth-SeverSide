b5b3e65111e1c97d77e311d3d8ef9c2dacd6307f
MohanReddy05 <mohanreddymk05@gmail.com>
2025-03-28 15:38:08 +0530

    MERN Auth Server-side

---
+++ controllers/userController.js
@@ -0,0 +1,19 @@
+import User from '../models/userModel.js';
+
+export const getUserData = async (req, res) => {
+    try {
+        const { userId } = req.body;
+        const user = await User.findById(userId);
+        if (!user) {
+            return res
+                .status(404)
+                .json({ result: false, message: 'User not found' });
+        }
+        return res.status(200).json({
+            result: true,
+            userData: { name: user.name, isVerified: user.isVerified },
+        });
+    } catch (error) {
+        return res.status(500).json({ result: false, message: error.message });
+    }
+};
